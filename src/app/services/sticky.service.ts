7144754e863e247468a3ea900714509ec619fe58
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2018-03-25 16:04:21 -0400

    Adding service comments

--- src/app/services/sticky.service.ts
+++ src/app/services/sticky.service.ts
@@ -5,15 +5,36 @@ import { Sticky } from './../models/sticky';
 @Injectable()
 export class StickyService {
 
-  stickies: Array<Sticky> = [];
+  /**
+   * Array of stickies.
+   *
+   * @property
+   * @private
+   */
+  stickies: Array<Sticky>;
 
+  /**
+   * Constructor.
+   */
   constructor() {
+    this.stickies = [];
   }
 
+  /**
+   * Returns all stickies.
+   *
+   * @return {Array} {Array of stickies}
+   */
   getStickies(): Array<Sticky> {
     return this.stickies;
   }
 
+  /**
+   * Creates new sticky.
+   *
+   * @param  {Sticky} sticky: Sticky {new sticky object}
+   * @return {Sticky} {saved sticky object}
+   */
   createSticky(sticky: Sticky = null): Sticky {
     let newSticky: Sticky = sticky ? sticky : new Sticky("Untitled Sticky", "");
     return newSticky;
