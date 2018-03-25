c9c72b55c12ac982f19c6a1b220d935478fc7e3a
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2018-03-25 15:36:49 -0400

    Adding app root component template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
+//Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
-
+//Components
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { StickyAreaComponent } from './sticky-area/sticky-area.component';
 import { StickyComponent } from './sticky/sticky.component';
-
 
 @NgModule({
   declarations: [
