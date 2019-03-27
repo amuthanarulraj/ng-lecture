0ea4ffed49c9189df0a643e2e847ed6134dc369b
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2019-03-27 17:26:51 -0400

    Adding support for environment

--- src/app/common/app-constants.ts
+++
@@ -1,3 +0,0 @@
-export class AppConstants {
-    public static STICKY_SERVER_BASE_URL: string = 'http://localhost:3000';
-}
--- src/app/services/sticky.service.ts
+++ src/app/services/sticky.service.ts
@@ -2,7 +2,7 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Sticky } from './../models/sticky';
 
-import { AppConstants } from './../common/app-constants';
+import { environment } from './../../environments/environment';
 import { Observable } from 'rxjs';
 
 @Injectable()
@@ -16,13 +16,13 @@ export class StickyService {
    */
   constructor(private http: HttpClient) {
     this.stickyResource = 'stickies';
-    this.stickyResourceURL = `${AppConstants.STICKY_SERVER_BASE_URL}/${this.stickyResource}`;
+    this.stickyResourceURL = `${environment.serverBaseURL}/${this.stickyResource}`;
   }
 
   /**
    * Returns all stickies.
    *
-   * @return {Observable} {Observable sticky array of stickies}
+   * @return {Observable<Array<Sticky>>} {Observable sticky array of stickies}
    */
   getStickies(): Observable<Array<Sticky>> {
     return this.http.get<Array<Sticky>>(this.stickyResourceURL);
@@ -32,7 +32,7 @@ export class StickyService {
    * Creates new sticky.
    *
    * @param  {Sticky} sticky: Sticky {new sticky object}
-   * @return {Observable} {Observable for saved sticky object}
+   * @return {Observable<Sticky>} {Observable for saved sticky object}
    */
   createSticky(sticky: Sticky = null): Observable<Sticky> {
     let newSticky: Sticky;
