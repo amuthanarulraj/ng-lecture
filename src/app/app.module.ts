45027d4b54c65d3f978ad99b1271777fb2a8312b
Amuthan Arulraj <arulraj.amuthan@gmail.com>
2020-04-05 11:22:31 -0400

    Adding translate

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,9 +1,9 @@
 // Modules
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
-import { HttpClientModule } from '@angular/common/http';
+import { HttpClientModule, HttpClient } from '@angular/common/http';
 import { Routes, RouterModule } from '@angular/router';
-import { NgModule, CUSTOM_ELEMENTS_SCHEMA } from '@angular/core';
+import { NgModule, CUSTOM_ELEMENTS_SCHEMA, APP_INITIALIZER } from '@angular/core';
 // Components
 import { AppComponent } from './app.component';
 import { NavBarComponent } from './nav-bar/nav-bar.component';
@@ -18,6 +18,10 @@ import { StoreModule } from '@ngrx/store';
 import { reducers } from './store/reducers';
 import { environment } from '../environments/environment';
 import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+
+import { TranslateModule, TranslateLoader, TranslateService } from '@ngx-translate/core';
+import { translationLoaderFactory } from './common/i18n/translate.loader';
+import { translationFactory } from './common/i18n/translate.initializer';
 
 const routes: Routes = [
   {
@@ -40,9 +44,24 @@ const routes: Routes = [
     HttpClientModule,
     StoreModule.forRoot(reducers, { }),
     RouterModule.forRoot(routes),
+    TranslateModule.forRoot({
+      loader: {
+        provide: TranslateLoader,
+        useFactory: translationLoaderFactory,
+        deps: [ HttpClient ],
+      }
+    }),
     !environment.production ? StoreDevtoolsModule.instrument({ name: 'Sticky Dev Tools'}) : []
   ],
-  providers: [ StickyService ],
+  providers: [
+    StickyService,
+    {
+      provide: APP_INITIALIZER,
+      useFactory: translationFactory,
+      deps: [ TranslateService ],
+      multi: true
+    }
+  ],
   bootstrap: [AppComponent],
   schemas: [ CUSTOM_ELEMENTS_SCHEMA ]
 })
